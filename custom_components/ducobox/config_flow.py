5bf16822071f4978c5bebcffcaad3c1194309f8d
Stijn Geens <33414262+degeens@users.noreply.github.com>
2026-03-04 21:32:14 +0100

    Add Zeroconf discovery support (#39)

--- custom_components/ducobox/config_flow.py
+++ custom_components/ducobox/config_flow.py
@@ -7,14 +7,14 @@ from typing import Any
 
 import voluptuous as vol
 from aiohttp import ClientError
+from homeassistant.components import zeroconf
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_HOST
-from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import DucoConnectivityBoardApi
 from .const import DOMAIN
+from .models import DucoBoxDeviceInfo
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,54 +25,89 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, str]:
-    """
-    Validate the user input allows us to connect.
-
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-    """
-    session = async_get_clientsession(hass)
-    api = DucoConnectivityBoardApi(data[CONF_HOST], session)
-
-    try:
-        device_info = await api.async_get_device_info()
-    except ClientError as err:
-        raise CannotConnectError from err
-
-    return {"title": device_info.model, "unique_id": device_info.serial_number}
-
-
 class DucoBoxConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for DucoBox."""
 
     VERSION = 1
     MINOR_VERSION = 1
 
-    async def async_step_user(
+    _zeroconf_discovered_host: str
+    _zeroconf_discovered_model: str
+
+    async def async_step_zeroconf(
+        self, info: zeroconf.ZeroconfServiceInfo
+    ) -> ConfigFlowResult:
+        """Handle the config flow initiated by Zeroconf."""
+        host = info.host
+
+        try:
+            device_info = await self._async_get_device_info(host)
+        except ClientError:
+            _LOGGER.exception(
+                "Failed to connect to Duco Connectivity Board at %s", host
+            )
+            return self.async_abort(reason="cannot_connect")
+
+        await self.async_set_unique_id(device_info.serial_number)
+        self._abort_if_unique_id_configured(updates={CONF_HOST: host})
+
+        self._zeroconf_discovered_host = host
+        self._zeroconf_discovered_model = device_info.model
+
+        return await self.async_step_zeroconf_confirm()
+
+    async def async_step_zeroconf_confirm(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
-        """Handle the initial step."""
+        """Handle the Zeroconf config flow confirmation step."""
+        if user_input is not None:
+            return self.async_create_entry(
+                title=self._zeroconf_discovered_model,
+                data={CONF_HOST: self._zeroconf_discovered_host},
+            )
+
+        return self.async_show_form(
+            step_id="zeroconf_confirm",
+            description_placeholders={
+                "name": self._zeroconf_discovered_model,
+                "host": self._zeroconf_discovered_host,
+            },
+        )
+
+    async def async_step_user(
+        self, info: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the config flow initiated by the user."""
         errors: dict[str, str] = {}
 
-        if user_input is not None:
+        if info is not None:
+            host = info[CONF_HOST]
+
             try:
-                info = await validate_input(self.hass, user_input)
-            except CannotConnectError:
+                device_info = await self._async_get_device_info(host)
+            except ClientError:
+                _LOGGER.exception(
+                    "Failed to connect to Duco Connectivity Board at %s", host
+                )
                 errors["base"] = "cannot_connect"
             except Exception:
-                _LOGGER.exception("Unexpected exception")
+                _LOGGER.exception(
+                    "Unexpected error connecting to Duco Connectivity Board at %s", host
+                )
                 errors["base"] = "unknown"
             else:
-                await self.async_set_unique_id(info["unique_id"])
+                await self.async_set_unique_id(device_info.serial_number)
+                self._abort_if_unique_id_configured(updates={CONF_HOST: host})
 
-                self._abort_if_unique_id_configured()
-
-                return self.async_create_entry(title=info["title"], data=user_input)
+                return self.async_create_entry(
+                    title=device_info.model, data={CONF_HOST: host}
+                )
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
-
-class CannotConnectError(HomeAssistantError):
-    """Error to indicate we cannot connect."""
+    async def _async_get_device_info(self, host: str) -> DucoBoxDeviceInfo:
+        session = async_get_clientsession(self.hass)
+        api = DucoConnectivityBoardApi(host, session)
+        return await api.async_get_device_info()
